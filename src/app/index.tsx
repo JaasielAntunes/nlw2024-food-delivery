5b622f7f7497899e02894c8411d31fa2847330b6
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-09 13:37:49 -0300

    Componente header

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text } from 'react-native'
+import { Header } from '@/components/header'
+import { View } from 'react-native'
 
 export default function Home() {
   return (
-    <View className="bg-slate-900 flex-1">
-      <Text className="text-white">Iniciando aplicação...</Text>
+    <View className="flex-1 pt-14">
+      <Header title="Faça seu pedido" />
     </View>
   )
 }
