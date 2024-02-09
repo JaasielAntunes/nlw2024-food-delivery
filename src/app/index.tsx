f7ebea00c34ef159dfc8c03fbedb00d7bb36078f
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-02-09 14:06:22 -0300

    Categoria e seleção com estados (useState)

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,10 +1,35 @@
-import { Header } from '@/components/header'
-import { View } from 'react-native'
+import { useState } from 'react';
+import { View, FlatList } from 'react-native';
+import { Header } from '@/components/header';
+import { CategoryButton } from '@/components/category-btn';
+import { CATEGORIES } from '@/utils/data/products';
 
 export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0]);
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory);
+  }
+
   return (
     <View className="flex-1 pt-14">
-      <Header title="Faça seu pedido" />
+      <Header title="Faça seu pedido" cartQuantityItems={1} />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className="max-h-10 mt-5"
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
     </View>
-  )
+  );
 }
